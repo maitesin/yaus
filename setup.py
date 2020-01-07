f56baafe3d3f1b8944d89073b26946ea80fe0cba
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2020-01-07 18:43:19 +0000

    Add basic CI and tox functionality

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='short',
+      version='0.1',
+      description='URL shortener service',
+      author='Oscar Forner Martinez',
+      author_email='oscar.forner.martinez@gmail.com',
+      url='https://www.gitlab.com/maitesin/short',
+      py_modules=['short']
+     )
