24a0691e430d7b0581854546db8b5d9effea7f0a
Oscar Forner Martinez <oscar.forner@globalsign.com>
2020-01-22 06:53:04 +0000

    Fix usage of Configuration type

--- yaus/__init__.py
+++ yaus/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
 
