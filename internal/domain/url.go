5edc31350c55e8290c0f9486a26284033b743673
Oscar Forner Martinez <oscar.forner@paack.co>
2021-04-18 17:03:57 +0200

    Add URL to the domain

--- internal/domain/url.go
+++ internal/domain/url.go
@@ -1,1 +1,49 @@
 package domain
+
+import (
+	"math/rand"
+	"time"
+)
+
+//go:generate moq -out mock_test.go -pkg domain_test . TimeProvider
+
+// TimeProvider defines the way to obtain the current time
+type TimeProvider interface {
+	Now() time.Time
+}
+
+// TimeProviderUTC provides time in UTC
+type TimeProviderUTC struct{}
+
+// Now returns the current time
+func (tpu *TimeProviderUTC) Now() time.Time {
+	return time.Now().UTC()
+}
+
+// URL contains the information for the shortened URL
+type URL struct {
+	Original  string
+	Shortened string
+	CreatedAt time.Time
+}
+
+// NewURL is a constructor
+func NewURL(tp TimeProvider, original string) URL {
+	t := tp.Now().UTC()
+	return URL{
+		Original:  original,
+		Shortened: generateRandomString(t.UnixNano(), 12),
+		CreatedAt: t,
+	}
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func generateRandomString(seed int64, size uint8) string {
+	rand.Seed(seed)
+	b := make([]byte, size)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
--- internal/domain/url_test.go
+++ internal/domain/url_test.go
@@ -1,1 +1,42 @@
-package domain
+package domain_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/maitesin/yaus/internal/domain"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewURL(t *testing.T) {
+	tests := []struct {
+		name         string
+		timeProvider domain.TimeProvider
+		url          string
+		expected     domain.URL
+	}{
+		{
+			name: "",
+			timeProvider: &TimeProviderMock{
+				NowFunc: func() time.Time {
+					return time.Time{}
+				},
+			},
+			url: "https://oscarforner.com",
+			expected: domain.URL{
+				Shortened: "SRZqe6P56fHw",
+				Original:  "https://oscarforner.com",
+				CreatedAt: time.Time{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := domain.NewURL(tt.timeProvider, tt.url)
+			require.Equal(t, tt.expected, got)
+		})
+	}
+}
